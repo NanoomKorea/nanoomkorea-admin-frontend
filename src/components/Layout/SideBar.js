1deaa51fb5cbf05e4cbafb4d0a9f7af338e3f685
NiNinanana <yeen00123@gmail.com>
2022-05-14 15:26:08 +0900

    polaris frame 생성

---
+++ src/components/Layout/SideBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navigation } from "@shopify/polaris";
+import { HomeMinor, OrdersMinor, ProductsMinor } from "@shopify/polaris-icons";
+
+export default function SideBarFrame() {
+  return (
+    <Navigation location="/">
+      <Navigation.Section
+        items={[
+          {
+            url: "/",
+            label: "Home",
+            icon: HomeMinor,
+          },
+          {
+            url: "/path/to/place",
+            label: "Orders",
+            icon: OrdersMinor,
+            badge: "15",
+          },
+          {
+            url: "/path/to/place",
+            label: "Products",
+            icon: ProductsMinor,
+          },
+        ]}
+      />
+    </Navigation>
+  );
+}
---
+++ src/components/Layout/TopBar.js
@@ -0,0 +1,102 @@
+import React, { useCallback, useState } from "react";
+import { ActionList, Icon, TopBar, VisuallyHidden } from "@shopify/polaris";
+import { ArrowLeftMinor, QuestionMarkMajor } from "@shopify/polaris-icons";
+
+export default function TopBarFrame() {
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+  const [isSecondaryMenuOpen, setIsSecondaryMenuOpen] = useState(false);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const toggleIsUserMenuOpen = useCallback(
+    () => setIsUserMenuOpen((isUserMenuOpen) => !isUserMenuOpen),
+    []
+  );
+
+  const toggleIsSecondaryMenuOpen = useCallback(
+    () => setIsSecondaryMenuOpen((isSecondaryMenuOpen) => !isSecondaryMenuOpen),
+    []
+  );
+
+  const handleSearchResultsDismiss = useCallback(() => {
+    setIsSearchActive(false);
+    setSearchValue("");
+  }, []);
+
+  const handleSearchChange = useCallback((value) => {
+    setSearchValue(value);
+    setIsSearchActive(value.length > 0);
+  }, []);
+
+  const handleNavigationToggle = useCallback(() => {
+    console.log("toggle navigation visibility");
+  }, []);
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      actions={[
+        {
+          items: [{ content: "Back to Shopify", icon: ArrowLeftMinor }],
+        },
+        {
+          items: [{ content: "Community forums" }],
+        },
+      ]}
+      name="Dharma"
+      detail="Jaded Pixel"
+      initials="D"
+      open={isUserMenuOpen}
+      onToggle={toggleIsUserMenuOpen}
+    />
+  );
+
+  const searchResultsMarkup = (
+    <ActionList
+      items={[
+        { content: "Shopify help center" },
+        { content: "Community forums" },
+      ]}
+    />
+  );
+
+  const searchFieldMarkup = (
+    <TopBar.SearchField
+      onChange={handleSearchChange}
+      value={searchValue}
+      placeholder="Search"
+      showFocusBorder
+    />
+  );
+
+  const secondaryMenuMarkup = (
+    <TopBar.Menu
+      activatorContent={
+        <span>
+          <Icon source={QuestionMarkMajor} />
+          <VisuallyHidden>Secondary menu</VisuallyHidden>
+        </span>
+      }
+      open={isSecondaryMenuOpen}
+      onOpen={toggleIsSecondaryMenuOpen}
+      onClose={toggleIsSecondaryMenuOpen}
+      actions={[
+        {
+          items: [{ content: "Community forums" }],
+        },
+      ]}
+    />
+  );
+
+  return (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+      secondaryMenu={secondaryMenuMarkup}
+      searchResultsVisible={isSearchActive}
+      searchField={searchFieldMarkup}
+      searchResults={searchResultsMarkup}
+      onSearchResultsDismiss={handleSearchResultsDismiss}
+      onNavigationToggle={handleNavigationToggle}
+    />
+  );
+}
